247d6d7d2bdd5a5020516f48e95a35a7b1ae56c1
Luke Redpath <luke@lukeredpath.co.uk>
2022-01-04 17:44:22 +0000

    Initial Commit

---
+++ TCANavigation/TCANavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCANavigationApp.swift
+//  TCANavigation
+//
+//  Created by Luke Redpath on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TCANavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
